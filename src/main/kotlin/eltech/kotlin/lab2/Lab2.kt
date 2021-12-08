afa5065ef486d541a108e4c218ec6c7ca9f1c0bc
voov4ka <vovsfury@gmail.com>
2021-12-08 19:10:48 +0300

    Added DoMath and opOrBracket functions

--- src/main/kotlin/eltech/kotlin/lab2/Lab2.kt
+++ src/main/kotlin/eltech/kotlin/lab2/Lab2.kt
@@ -1,6 +1,7 @@
 package eltech.kotlin.lab2
 
 import java.util.ArrayDeque
+import kotlin.math.pow
 
 val operator = arrayOf('+', '-', '*', '/', '^', '_')
 // Common used operators and unary minus ("negative value")
@@ -42,6 +43,28 @@ fun postfixCalc (InFormula: String): Int {
     return numStack.removeLast() // last number -> total result
 }
 
+fun doMath(numStack: ArrayDeque<Int>, op: Char) {
+
+    when (op) {
+        operator[0] -> numStack.add(numStack.removeLast() + numStack.removeLast())
+        operator[1] -> numStack.add(numStack.removeLast() - numStack.removeLast())
+        operator[2] -> numStack.add(numStack.removeLast() * numStack.removeLast())
+        operator[3] -> {
+            val denominator = numStack.removeLast() // we are extracting numbers in reverse
+            val numerator = numStack.removeLast() // bc of the stack structure
+            numStack.add(numerator / denominator)
+        }
+        operator[4] -> {
+            val power = numStack.removeLast() // we are extracting numbers in reverse
+            val base = numStack.removeLast().toDouble() // bc of the stack structure
+            numStack.add(base.pow(power).toInt())
+        }
+        operator[5] -> numStack.add(0 - numStack.removeLast())
+        else -> throw IllegalArgumentException("Unexpected symbol found: $op." +
+                "\nCannot process given formula")
+    }
+}
+
 fun infixToPostfix(InFormula: String): String {
     val opStack = ArrayDeque<Char>() // storage for operators
     var postFormula = ""
@@ -79,4 +102,30 @@ fun infixToPostfix(InFormula: String): String {
         postFormula += opStack.removeLast()
     }
     return postFormula
+}
+
+fun opOrBracket (symbol: Char): Boolean {
+    return ((symbol in operator) || (symbol == Lbracket))
+}
+
+fun opAdd (currentChar: Char, opStack: ArrayDeque<Char>, postFormula: String): String {
+    val minPriority = operator.indexOf(currentChar)
+    var resultFormula = postFormula + ' ' // add space to separate from previous symbol
+
+    while (opStack.isNotEmpty() && (operator.indexOf(opStack.peekLast()) >= minPriority)) {
+        resultFormula += opStack.removeLast() // adding all operators to formula with higher
+    }
+    opStack.add(currentChar)
+    return resultFormula
+}
+
+fun bracketFlush(opStack: ArrayDeque<Char>, postFormula: String): String {
+    var tempChar = opStack.removeLast()
+    var resultFormula = postFormula
+
+    while (tempChar != Lbracket) {
+        resultFormula += tempChar // adding all leftover operators inside brackets
+        tempChar = opStack.removeLast()
+    }
+    return resultFormula
 }
