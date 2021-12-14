ec1e9ecd08ee6b2234c05b036a3167efe56ac247
Void-Rei_PC <oelga.isakov@gmail.com>
2021-12-14 18:56:57 +0300

    Fixed matrix initialization and implemented set, get, toString

--- src/main/kotlin/eltech/kotlin/lab4/lab4.kt
+++ src/main/kotlin/eltech/kotlin/lab4/lab4.kt
@@ -4,16 +4,20 @@ fun main() {
 
 }
 
-class Matrix(newValues: MutableList<MutableList<Int>>) {
+class Matrix(newValues: MutableList<MutableList<Double>>) {
     var matrix = newValues
         set(setValues) {
             field = setValues
-            rows = setValues.lastIndex
-            lines = setValues[0].lastIndex
+            lines = setValues.lastIndex
+            rows = setValues[0].lastIndex
         }
 
-    private var rows = newValues.lastIndex
-    private var lines = newValues[0].lastIndex
+    private var lines = newValues.lastIndex
+    private var rows = newValues[0].lastIndex
+
+    fun getSizes(): Pair<Int, Int> {
+        return Pair(lines, rows)
+    }
 
     operator fun plus(other: Matrix): Matrix {
         TODO("Not yet implemented")
@@ -31,12 +35,21 @@ class Matrix(newValues: MutableList<MutableList<Int>>) {
         TODO("Not yet implemented")
     }
 
-    operator fun set(i: Int, j: Int, value: Double) {
-        TODO("Not yet implemented")
+    private fun wrongIndexCheck(i: Int, j: Int)
+    {
+        if (j > rows) throw IndexOutOfBoundsException ("Index j should be less than rows amount!")
+        if (i > lines) throw IndexOutOfBoundsException ("Index i should be less than lines amount!")
+        if (j < 0) throw IndexOutOfBoundsException ("Index j should be more than 0!")
+        if (i < 0) throw IndexOutOfBoundsException ("Index i should be more than 0!")
     }
 
-    operator fun get(i: Int, j: Int): Double {
-        TODO("Not yet implemented")
+    operator fun set(line: Int, row: Int, value: Double) {
+        wrongIndexCheck(line, row)
+        matrix[line][row] = value
+    }
+
+    operator fun get(line: Int, row: Int): Double {
+        return matrix[line][row]
     }
 
     operator fun unaryMinus(): Matrix {
@@ -46,5 +59,16 @@ class Matrix(newValues: MutableList<MutableList<Int>>) {
     operator fun unaryPlus(): Matrix {
         return this
     }
-    // ...
+
+    override fun toString(): String {
+        var resultString = ""
+
+        for (i in 0..lines) {
+            for (j in 0..rows) {
+                resultString += matrix[i][j].toString() + ' '
+            }
+            resultString += System.lineSeparator()
+         }
+        return resultString
+    }
 }
