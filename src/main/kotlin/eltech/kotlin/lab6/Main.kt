e7f3c7a1be1c3b9b9949f052ff0f531f34794b62
Void-Rei_PC <oelga.isakov@gmail.com>
2021-12-27 19:03:13 +0300

    lab 6 fixed
    * Swapped main and ShapeCollector to correct files

--- src/main/kotlin/eltech/kotlin/lab6/Main.kt
+++ src/main/kotlin/eltech/kotlin/lab6/Main.kt
@@ -1,21 +1,5 @@
 package eltech.kotlin.lab6
-import eltech.kotlin.lab3.*
 
-class ShapeCollector<T /* ??? */ > {
-    private val allShapes = mutableListOf<T>()
+fun main() {
 
-    fun add(new: T) {
-    /* ??? */
-    }
-    fun addAll(new: /* ??? */ T) {
-    /* ??? */
-    }
-
-    fun getAll(): List<T> {
-        return allShapes
-    }
-    fun getAllSorted(comparator: /* ??? */ T): List<T> {
-    /* ??? */
-        return allShapes
-    }
 }
